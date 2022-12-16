69b5e6a48c7cdec6899453e560ef7dc85da26eeb
Gadomskyy <j_gadomski@hotmail.com>
2022-12-16 23:11:44 +0100

    Connected push buttons
    Established connection to two push buttons

--- gui_launcher.py
+++ gui_launcher.py
@@ -7,9 +7,14 @@ class Ui(QtWidgets.QMainWindow):
         uic.loadUi('qr_code_gui.ui', self)
         self.show()
 
+        #Connecting the Save, Load, Exit options from File menu to methods
         self.actionSave.triggered.connect(self.save_qr)
         self.actionLoad.triggered.connect(self.load_qr)
         self.actionExit.triggered.connect(self.exit_program)
+
+        #Connecting the push buttons to menus
+        self.convert_into_QR.clicked.connect(self.make_qr)
+        self.convert_into_text.clicked.connect(self.make_text)
 
     def save_qr(self):
         pass
@@ -19,6 +24,12 @@ class Ui(QtWidgets.QMainWindow):
 
     def exit_program(self):
         sys.exit()
+
+    def make_qr(self):
+        pass
+
+    def make_text(self):
+        pass
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
