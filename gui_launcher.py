972580bd5e500327e9ef22125db8e175bb2aa34d
Gadomskyy <j_gadomski@hotmail.com>
2022-12-16 23:05:19 +0100

    Menu option connections, exit implemented
    Added connections to Save, Load, Exit function from File menu, implemented Exit function

--- gui_launcher.py
+++ gui_launcher.py
@@ -7,6 +7,19 @@ class Ui(QtWidgets.QMainWindow):
         uic.loadUi('qr_code_gui.ui', self)
         self.show()
 
+        self.actionSave.triggered.connect(self.save_qr)
+        self.actionLoad.triggered.connect(self.load_qr)
+        self.actionExit.triggered.connect(self.exit_program)
+
+    def save_qr(self):
+        pass
+
+    def load_qr(self):
+        pass
+
+    def exit_program(self):
+        sys.exit()
+
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 app.exec_()
